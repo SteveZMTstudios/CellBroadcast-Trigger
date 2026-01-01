8e6b7bbfe35249ca9bcab8540fbf752b318d8551
stevezmtstudios <98326195+SteveZMTstudios@users.noreply.github.com>
2026-01-01 23:59:55 +0800

    feat: 添加 Android 构建和发布工作流

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "top.stevezmt.cellbroadcast.trigger"
         minSdk = 21
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
